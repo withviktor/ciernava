bc277a31c4174bfb69dd065532a267c11f7ee7dd
Viktor Čierňava <hello@ciernava.cz>
2025-03-30 08:20:19 +0200

    Refactor sitemap configuration by removing outdated URLs and adjusting change frequency and priority for improved SEO

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,20 +5,8 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://withviktor.com/",
       lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 1,
-    },
-    {
-      url: "https://withviktor.com/products",
-      lastModified: new Date(),
       changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://withviktor.com/blog",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.5,
+      priority: 1,
     },
   ];
 }
