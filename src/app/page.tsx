07ce517cac2f7f6e0ea1bd91f1ca271684981a27
Viktor Čierňava <viktor.ciernava@gdg-solutions.com>
2025-09-11 12:25:12 +0200

    Enhance HomePage component by adding a link for collaboration requests, updating the CustomButton to support child elements, and modifying the background styling for better visual appeal.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { ArrowRight } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 import myPhoto from "~/../public/assets/2FFDA69D-7ECA-40C5-9E88-34F5C964209B_1_105_c-removebg-preview.png";
 import { BlurFade } from "~/components/magicui/blur-fade";
@@ -23,11 +24,11 @@ export const metadata = getSEOTags({
     "web developer",
     "web designer",
   ],
-})
+});
 
 export default function HomePage() {
   return (
-    <div className="mt-0 flex min-h-screen w-full flex-col items-center justify-center p-4 sm:p-8">
+    <div className="mt-0 flex min-h-screen w-full flex-col items-center justify-center p-4 sm:p-8 bg-orange-50/50 dark:bg-gray-900">
       <div className="flex w-full max-w-[1300px] flex-row items-center justify-center gap-20">
         <BlurFade className="flex flex-col items-start gap-4">
           <SocialIcons />
@@ -44,13 +45,15 @@ export default function HomePage() {
             </span>
           </p>
 
-          <p className="mb-10 text-2xl font-light text-gray-800">
+          <p className="mb-10 text-2xl font-light">
             Zaobírám se tvorbou webových stránek a růstem Vaší značky.
           </p>
 
-          <CustomButton>
-            Ověřit dostupnost
-            <ArrowRight className="ml-2" />
+          <CustomButton asChild>
+            <Link href="mailto:business@withviktor.com">
+              Žádost o spolupráci
+              <ArrowRight className="ml-2" />
+            </Link>
           </CustomButton>
         </BlurFade>
       </div>
